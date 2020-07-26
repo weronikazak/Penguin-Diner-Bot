ea49f8bf2e0d0a9dd046a90ebb87645097efe1a4
Weronika Żak <weronikarzak@gmail.com>
2020-07-27 01:03:19 +0200

    create bot

--- bot.py
+++ bot.py
@@ -1,98 +1,115 @@
 import os
 import pyautogui
-import sys
 import time
-import cv2
+import sys
 
-input("Move mouse over bot window and press enter.")
+class PenguinDinterBot():
+	# ------------------
+	# INITIATE VARIABLES
+	# ------------------
+	def __init__(self):
+		ORDERS_PATH = os.getcwd() + "/orders"
+		MEALS_PATH = os.getcwd() + "/meals"
 
-w = pyautogui.locateOnScreen("locate.png")
-if w is None:
-	sys.exit("Couldn't find on screen. Is this game visible?")
-
-win_right = w[0]
-win_bottom = w[1]
-
-print("------GAME FOUND-------")
+		self.back_button = (750, 640)
+		self.trash_bin = (550, 650)
+		self.upgrade_button = (530, 550)
+		self.new_level = (530-150, 550)
 
 
-# -----------------------
-# UPGRADE MENU CONSTANTS
-# -----------------------
-upgrade_x_offset = 200
-upgrade_y_offset = 150
-upgrade_x_start = 300
-upgrade_y_start = 250
+		self.ORDERS = [ORDERS_PATH + "/" + order for order in os.listdir(ORDERS_PATH)]
+		self.MEALS = [MEALS_PATH + "/" + meal for meal in os.listdir(MEALS_PATH)]
 
-back_button = (750, 640)
-trash_bin = (550, 650)
+		self.MEAL2ORDER = dict()
+		for i, meal in enumerate(self.MEALS):
+			order = self.ORDERS[i]
+			self.MEAL2ORDER[meal] = order
 
-# ------------
-# START GAME
-# ------------
-pyautogui.click(win_right - 60, win_bottom - 180, interval=1)
-pyautogui.click(win_right - 280, win_bottom - 200, interval=1) # in case if game was started earlier
-pyautogui.click(back_button, interval=1)
-pyautogui.click(back_button, interval=1)
+		self.start_game()
 
-time.sleep(6)
 
-# ------------
-# START ORDERS
-# ------------
+	# ------------------
+	# FIRST CUTSCENE
+	# ------------------
+	def cutscene_start(self):
+		input("Move mouse over bot window and press enter.")
 
-CLIENTS = []
+		w = pyautogui.locateOnScreen("locate.png")
+		if w is None:
+			sys.exit("Couldn't find on screen. Is this game visible?")
 
-ORDERS_PATH = os.getcwd() + "/orders"
-MEALS_PATH = os.getcwd() + "/meals"
+		win_right = w[0]
+		win_bottom = w[1]
 
-ORDERS = [ORDERS_PATH + "/" + order for order in os.listdir(ORDERS_PATH)]
-MEALS = [MEALS_PATH + "/" + meal for meal in os.listdir(MEALS_PATH)]
+		print("------GAME FOUND-------")
 
-MEAL2ORDER = dict()
-for i, meal in enumerate(MEALS):
-	order = ORDERS[i]
-	MEAL2ORDER[meal] = order
+		pyautogui.click(win_right - 60, win_bottom - 180, interval=1)
+		pyautogui.click(win_right - 280, win_bottom - 200, interval=1) # in case if game was started earlier
+		pyautogui.click(self.back_button, interval=1)
+		pyautogui.click(self.back_button, interval=1)
 
-# LOCATE CLIENTS
-while True:
-	# if the level is accomplished
-	endgame = pyautogui.locateCenterOnScreen("end.png", grayscale=True, confidence=.9)
-	if endgame is not None:
+		time.sleep(6)
+
+
+	# ------------------
+	#  LEVEL END
+	# ------------------
+	def end_level(self):
 		print("------END OF THE DAY!------")
-		# first, look for upgrades
 		time.sleep(3)
-		upgrades = pyautogui.locateCenterOnScreen("upgrades.png", confidence=.9)
+		
+		# first, look for upgrades
+		self.upgrade()
 
-		pyautogui.click(upgrades[0], upgrades[1],interval=.3)
+		# start next level if available
+		self.start_level_or_restart()
 
-		# click everything
+		time.sleep(6)
+
+
+	# ------------------
+	# EQUIPMENT UPGRADES
+	# ------------------
+	def upgrade(self):
+		pyautogui.click(self.upgrade_button,interval=.3)
+
+		upgrade_x_offset = 200
+		upgrade_y_offset = 150
+		upgrade_x_start = 300
+		upgrade_y_start = 250
+
+		# since it's impossible to deal with numbers, click everything
 		for x in range(3):
 			for y in range(3):
 				click_x = upgrade_x_start + x*upgrade_x_offset
 				click_y = upgrade_y_start + y*upgrade_y_offset
-				pyautogui.click(click_x, click_y, interval=.3)
+				pyautogui.click(click_x, click_y, interval=.2)
 
-		pyautogui.click(back_button, interval=.3)
+		pyautogui.click(self.back_button, interval=.3)
 
-		# start next level if available
+
+	# --------------------------
+	# START NEW LEVEL OR RESTART
+	# --------------------------
+	def start_level_or_restart(self):
 		next_option = pyautogui.locateCenterOnScreen("nextlevel.png", confidence=.9)
 		if next_option is None:
 			# if not, start a new day or restart a day
-			pyautogui.click(upgrades[0]-150, upgrades[1], interval=.3)
+			pyautogui.click(self.new_level, interval=.3)
 		else:
 			print("\n------NEXT LEVEL!------\n")
 			pyautogui.click(next_option, interval=1)
-			pyautogui.click(back_button, interval=1)
-			pyautogui.click(back_button, interval=1)
+			pyautogui.click(self.back_button, interval=1)
+			pyautogui.click(self.back_button, interval=1)
 
-		time.sleep(6)
-	else:
-		print("Looking for a client......")
+
+	# --------------------------
+	# LEAD CLIENT TO FREE TABLE
+	# --------------------------
+	def lead_to_table(self):
 		client = pyautogui.locateCenterOnScreen("client.png", confidence=.9, region=(120,300, 80, 200))
-
 		if client is not None:
-			print("Found a client and directed them to a table.")
+			print("Found a client and led them to a table.")
 			pyautogui.click(client[0], client[1])
 			
 			free_table = pyautogui.locateCenterOnScreen("empty_table.png", confidence=.8)
@@ -103,46 +120,96 @@ while True:
 			
 			pyautogui.click(free_table[0], free_table[1])
 
-		# if there's a client, that wants to order something, take his order
-		ordering_client = pyautogui.locateCenterOnScreen("client_order.png",grayscale=True,  confidence=.7)
+
+	# --------------------
+	# TAKE CLIENT'S ORDER
+	# --------------------
+	def take_order(self):
+		ordering_client = pyautogui.locateCenterOnScreen("client_order.png",  confidence=.7)
 
 		if ordering_client is not None:
 			print("A client wants to order something!")
 			pyautogui.click(ordering_client[0], ordering_client[1])
 
-		# check if there are any meals awaiting for being served
-		for meal in MEALS:
-				meal_centered = pyautogui.locateCenterOnScreen(meal, confidence=.9, region=(0, 600, 490, 80))
-				if meal_centered is not None:
-					pyautogui.click(meal_centered[0], meal_centered[1])
+		# check for mad clients
+		ordering_client = pyautogui.locateCenterOnScreen("mad_client.png",  confidence=.8)
+		if ordering_client is not None:
+			print("A mad client wants to order something!")
+			pyautogui.click(ordering_client[0], ordering_client[1])
 
-					# look for coords of the client, who ordered the meal
-					look_for_order = MEAL2ORDER[meal]
-					order_centered = pyautogui.locateCenterOnScreen(look_for_order, confidence=.8)
-					conf = .8
+	
+	# ---------------------
+	# SERVE MEAL TO CLIENT
+	# ---------------------
+	def serve_meal(self):
+		for meal in self.MEALS:
+			meal_centered = pyautogui.locateCenterOnScreen(meal, confidence=.9, region=(0, 600, 490, 80))
+			if meal_centered is not None:
+				pyautogui.click(meal_centered[0], meal_centered[1])
 
-					while order_centered is None:
-						conf -= .1
-						order_centered = pyautogui.locateCenterOnScreen(look_for_order, confidence=conf)
-					
-					pyautogui.click(order_centered[0], order_centered[1])
+				# find image reference
+				look_for_order = self.MEAL2ORDER[meal]
+				order_centered = pyautogui.locateCenterOnScreen(look_for_order, confidence=.8)
+				conf = .8
 
-					food_name = look_for_order.split("/")[-1][:-4]
+				# if program doesn't recognize order, look for it until it succeed
+				while order_centered is None:
+					conf -= .1
+					order_centered = pyautogui.locateCenterOnScreen(look_for_order, confidence=conf)
+				
+				pyautogui.click(order_centered[0], order_centered[1])
 
-					print(f"Serving the meal {food_name} to the client.")
-					time.sleep(0.4)
-					break
+				food_name = look_for_order.split("/")[-1][:-4]
+
+				print(f"Serving the meal {food_name} to the client.")
+				time.sleep(0.3)
+				break
 
 
-		# look whether there's money left. if ture, collect them and clean table
+	# ---------------
+	# COLLECT MONEY
+	# ---------------
+	def collect_money(self):
 		money = pyautogui.locateCenterOnScreen("money.png", confidence=.8)
 		if money is not None:
 			print("Money collected.")
 			pyautogui.click(money[0], money[1])
 
-
+		# if there are money left, but the main charater covers it, force to move
 		if pyautogui.locateCenterOnScreen("pickup.png", grayscale=True, confidence=.8) is not None:
-			pyautogui.click(trash_bin)
+			pyautogui.click(self.trash_bin)
 
-sys.exit()
 
+
+	# ---------------
+	# GAME FLOW
+	# ---------------
+	def run_game(self):
+		while True:
+			# if the level is accomplished
+			endgame = pyautogui.locateCenterOnScreen("end.png", grayscale=True, confidence=.9)
+			if endgame is not None:
+				self.end_level()
+			else:
+				print("Looking for a client......")
+				
+				self.lead_to_table()
+				self.take_order()
+				self.serve_meal()
+				self.collect_money()
+
+
+	# ---------------------------
+	#
+	#         GAME START
+	#
+	# ---------------------------
+	def start_game(self):
+		self.cutscene_start()
+		self.run_game()
+
+
+
+
+if __name__ == "__main__":
+	bot = PenguinDinterBot()
